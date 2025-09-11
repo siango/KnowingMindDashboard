af055176e4bba0cb88617d92016552e551e022b9
siango < GitHub>
2025-09-11 21:55:43 +0700

    [OSP] Deploy grouped console @ 2025-09-11 21:55:43

--- docs/config.env.js
+++
@@ -1,3 +0,0 @@
-﻿window.KMS = window.KMS || {};
-window.KMS.GAS_URL = "https://script.google.com/macros/s/AKfycbxD7leRzTLrk5U4WlTBYXS38wqg5Zxzm-8SrtpsPwOsO1ez9v8xnN3WN0VcFX0Yj3uU/exec";
-window.KMS.API_KEY = "";
--- summary.js
+++
@@ -1,1 +0,0 @@
-window.KM_SUMMARY={list:[],map:{}};
