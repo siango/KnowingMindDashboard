be3999a85f3343cb98e108b974357106851d4d40
SiaNGo <62171697+siango@users.noreply.github.com>
2025-09-12 21:40:22 +0700

    Update config.env.js

--- docs/config.env.js
+++ docs/config.env.js
@@ -1,3 +1,4 @@
 ﻿window.KMS = window.KMS || {};
-window.KMS.GAS_URL = "https://script.google.com/macros/s/AKfycbxD7leRzTLrk5U4WlTBYXS38wqg5Zxzm-8SrtpsPwOsO1ez9v8xnN3WN0VcFX0Yj3uU/exec";
+window.KMS.GAS_URL = "https://script.google.com/macros/s/AKfycbwLDAR5dFqWedJHWJt2jE0UJH1kTn_2xYoXlAhjSZuKpkcWMYEQhtKf1CCYJwRlU2l0/exec";
 window.KMS.API_KEY = "";
+
